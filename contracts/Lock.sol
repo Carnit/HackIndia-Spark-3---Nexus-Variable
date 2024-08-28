e07df60f3bf37feb4e95b8e957345005fc5863ec
Carnit <nlesh.barat10.8@gmail.com>
2024-08-28 09:36:24 +0530

    update,added files

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,34 +1,17 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+// contracts/Flux1NFT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract Flux1NFT is ERC721, Ownable {
+    uint256 public nextTokenId;
 
-    event Withdrawal(uint amount, uint when);
+    constructor() ERC721("Flux1NFT", "FLUX") Ownable() {}
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    function mint(address to) external onlyOwner {
+        _safeMint(to, nextTokenId);
+        nextTokenId++;
     }
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Link } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Flux-1 AI</h1>
+      <Link to="/generate">Start Generating Content</Link>
+    </div>
+  );
+}
+
+export default Home;
